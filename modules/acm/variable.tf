b5daeac242fff99919af01f77c97d9484594bd2f
Victor Ukariwe <victorukariwe@Victors-MacBook-Pro.local>
2025-10-23 00:55:10 -0400

    dns-update

--- modules/acm/variable.tf
+++ modules/acm/variable.tf
@@ -2,13 +2,13 @@
 variable "domain_name" {
   description = "Primary domain name for the certificate"
   type        = string
-  default     = "fusisoft.com"
+  default     = "cloudysoft1.com"
 }
 
 variable "san_domains" {
   description = "Subject alternative names for the certificate"
   type        = list(string)
-  default     = ["*.fusisoft.com"]
+  default     = ["*.cloudysoft1.com"]
 }
 
 variable "route53_zone_id" {
