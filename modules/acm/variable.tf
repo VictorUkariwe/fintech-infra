ad38e276913e1f338aeefba56e13ff8825e64bb2
Victor Ukariwe <victorukariwe@Victors-MacBook-Pro.local>
2025-10-23 01:13:26 -0400

    dns-hostedzoneid-update

--- modules/acm/variable.tf
+++ modules/acm/variable.tf
@@ -14,7 +14,7 @@ variable "san_domains" {
 variable "route53_zone_id" {
   description = "Route 53 Hosted Zone ID"
   type        = string
-  default     = "ZC7WDABJDII2" # Replace with actual Route 53 Zone ID
+  default     = "Z06296241FTWELDS1C0EW" # Replace with actual Route 53 Zone ID
 }
 
 variable "tags" {
